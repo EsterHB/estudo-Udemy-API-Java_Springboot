05ec6c6c6f5ff4c973814517302b5a28ef8d4bdb
Ester <ester.hb@hotmail.com>
2025-11-03 14:43:25 -0300

    Adição da API com CRUD básico

---
+++ src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,56 @@
+package io.github.cursodsousa.produtosapi.controller;
+
+import io.github.cursodsousa.produtosapi.model.Produto;
+import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController //marca a classe para ser um controlador rest, ou seja, vai receber as requisições
+@RequestMapping ("produtos") //determina qual é a url base para esse controller
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+
+    @PostMapping  //cria um recurso no servidor, como salvar algum dado
+    public Produto salvar (@RequestBody Produto produto){
+        UUID.randomUUID().toString();
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
+    }
+
+
+
+}
